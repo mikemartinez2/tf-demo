23ee73463d7168f41ec5855c9623b90abde72ec1
mikemartinez2 <mike@bruskland.com>
2024-12-30 14:12:19 -0600

    updated webpage body

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd
 sudo systemctl status httpd
-echo "<html><head><title>Hello from AWS!</title></head><body><h1>Hello from my Terraform Sales Demo!</h1></body></html>" | sudo tee /var/www/html/index.html > /dev/null
+echo "<html><head><title>Hello from my Terraform Sales Demo!</title></head><body><h1>Hello from my Terraform Sales Demo!</h1></body></html>" | sudo tee /var/www/html/index.html > /dev/null
 EOF
 }
 
